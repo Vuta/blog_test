8253b0d209db23bd91ae1578d718edf536b02c61
trananhvu <eyelight93@gmail.com>
2017-11-12 22:15:35 +0700

    create comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: {minimum: 8, maximum:  255}
   validates :content, presence: true, length: {maximum:  1000}
