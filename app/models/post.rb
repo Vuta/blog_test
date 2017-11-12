d23fb65570b289bb2e15dc6b19e95953eb26731d
trananhvu <eyelight93@gmail.com>
2017-11-13 06:57:51 +0700

    integrate ckeditor for blog post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: {minimum: 8, maximum:  255}
-  validates :content, presence: true, length: {maximum:  1000}
+  validates :content, presence: true, length: {minimum:  20}
 end
