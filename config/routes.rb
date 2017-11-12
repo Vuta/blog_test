032d8d6b15cf58fdb87c7cba930cdf285c61099e
trananhvu <eyelight93@gmail.com>
2017-11-12 22:40:43 +0700

    user can post comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :posts, except: [:edit, :update, :destroy]
+  resources :posts, except: [:edit, :update, :destroy] do
+    resources :comments, only: :create
+  end
 
   root "posts#index"
 end
