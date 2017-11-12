06bcdf79233a73db74f94cb1e4af13d3db9c5d8e
trananhvu <eyelight93@gmail.com>
2017-11-12 19:34:14 +0700

    seed data for user and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true, length: {minimum: 8, maximum:  255}
-  validates :content, presence: true, length: {maximum:  255}
+  validates :content, presence: true, length: {maximum:  1000}
 end
