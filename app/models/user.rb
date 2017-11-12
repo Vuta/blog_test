d550714ec4ff6a9f2c3c58089d7e0ca15d8f0cb4
trananhvu <eyelight93@gmail.com>
2017-11-12 17:14:42 +0700

    user, book validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, dependent: :destroy
+
+  validates :username, presence: true, length: {minimum: 8, maximum: 50}
+  validates :email, presence: true, , length: {maximum: 255}, uniqueness: {case_sensitive: false},  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
