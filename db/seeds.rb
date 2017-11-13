1b173f0348b8b1042b7f0dd0d35dec85c3a72da9
trananhvu <eyelight93@gmail.com>
2017-11-13 07:34:39 +0700

    change seed data implementation

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,18 @@
 insert_user = "INSERT INTO users (username, email, created_at, updated_at) VALUES "
 insert_post = "INSERT INTO posts (title, content, user_id, created_at, updated_at) VALUES "
 
+puts "Inserting data, this may take a while..."
 1000.times do |i|
   # replace ".", "_" character with space and format name
   name = Faker::Internet.unique.user_name(8..15).gsub(/[._]/, " ").titleize
-
   email = Faker::Internet.email(name)
-  insert_user << "(\'#{name}\', \'#{email}\', \'#{Time.now}\', \'#{Time.now}\'), "
+
+  rand_time = Time.now - rand(1..100).days
+  insert_user << "(\'#{name}\', \'#{email}\', \'#{rand_time}\', \'#{rand_time}\'), "
 
   50.times do
     post_title = Faker::Lorem.sentence
-    post_content = Faker::Lorem.paragraphs(5).join("\n")
+    post_content = Faker::Lorem.paragraphs(20).join("\n")
 
     insert_post << "(\'#{post_title}\', \'#{post_content}\', \'#{i + 1}\', \'#{Time.now}\', \'#{Time.now}\'), "
   end
@@ -26,8 +28,13 @@ end
 insert_user.chop!.chop! << ";" # remove the ", " at the end of query and add ";"
 insert_post.chop!.chop! << ";" # remove the ", " at the end of query and add ";"
 
+puts "Creating users..."
 ActiveRecord::Base.connection.execute(insert_user)
+puts "Done."
+
+puts "Creating posts..."
 ActiveRecord::Base.connection.execute(insert_post)
+puts "Done."
 
 # Update users password
 password = Devise::Encryptor.digest(User, '123456')
